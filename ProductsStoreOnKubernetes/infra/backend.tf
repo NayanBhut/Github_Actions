cf742ff246bb7413f5be82517feef0dc26ec0b42
Nayan Bhut <nayan.bhut@brainvire.com>
2025-04-08 21:40:29 +0530

    Updated backend.tf

--- ProductsStoreOnKubernetes/infra/backend.tf
+++ ProductsStoreOnKubernetes/infra/backend.tf
@@ -5,7 +5,7 @@ terraform {
     container_name         = "appcontainer"
     key                    = "terraform.tfstate"
     use_azuread_auth       = true
-    subscription_id        = var.subscription_id
-    tenant_id              = var.tenant_id
+    subscription_id        = "d482e246-47c2-4f4e-a7c2-309c3d138f3c"
+    tenant_id              = "e11a1a7b-d147-4ab8-9910-a146afab1089"
   }
 }
