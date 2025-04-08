13461c239982eaf82491e1fd82dbf86aecf4ca6d
Nayan Bhut <nayan.bhut@brainvire.com>
2025-04-08 21:38:09 +0530

    Updated Backend tf file

--- ProductsStoreOnKubernetes/infra/backend.tf
+++ ProductsStoreOnKubernetes/infra/backend.tf
@@ -1,4 +1,11 @@
 terraform {
   backend "azurerm" {
+    resource_group_name     = "rg-test-resourse-group"
+    storage_account_name   = "aksstorageacco"
+    container_name         = "appcontainer"
+    key                    = "terraform.tfstate"
+    use_azuread_auth       = true
+    subscription_id        = var.subscription_id
+    tenant_id              = var.tenant_id
   }
 }
