bf6c526b3ec9431c5cc11b4db83344bb9c8732b8
Nayan Bhut <nayan.bhut@brainvire.com>
2025-04-08 21:28:02 +0530

    Reverted changes

--- ProductsStoreOnKubernetes/infra/backend.tf
+++ ProductsStoreOnKubernetes/infra/backend.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = var.storage_name
-    container_name       = var.appContainerName
-    key                  = "terraform.tfstate"
-    access_key           = var.access_key
   }
 }
